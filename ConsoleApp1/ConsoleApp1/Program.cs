6ba3f1551bcad053a224230e9edce8cc34539c06
IDK7234 <clewe2008@mail.ru>
2024-05-23 20:13:35 +0300

    added iinheritance examples

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using static System.Console;
+
+namespace Hell
+{
+    public class Human
+    {
+        string first_name, last_name, town;
+        DateTime date;
+        public Human() { }
+
+        public virtual void Show()
+        {
+            WriteLine($"Фамилия {last_name}, Имя {first_name}, Дата рождения {date.ToShortDateString()}");
+        }
+        public void SetFirstName(string name) { first_name = name; }
+        public void SetLastName(string name) { last_name = name; }
+        public void SetBirthDate(DateTime name) { date = name; }
+        public void SetBirthTown(string name) { town = name; }
+    }
+
+    public class Employee : Human
+    {
+        double salary;
+        public Employee() { }
+
+        public override void Show()
+        {
+            base.Show();
+            WriteLine($", Зароботная плата {salary}\n");
+        }
+        public void SetSalary(double sal) { salary = sal; }
+    }
+
+    public class EmployeeBilder
+    {
+        Employee emp = new Employee();
+        public EmployeeBilder SetFirstName(string value) { emp.SetFirstName(value); return this; }
+        public EmployeeBilder SetLastName(string value) { emp.SetLastName(value); return this; }
+        public EmployeeBilder SetBirthDate(DateTime value) { emp.SetBirthDate(value); return this; }
+        public EmployeeBilder SetSalary(double sal) { emp.SetSalary(sal); return this; }
+        public EmployeeBilder SetBirthTown(string name) { emp.SetBirthTown(name); return this; }
+
+        public Employee build()
+        {
+            return emp;
+        }
+
+    }
+
+    public class Scientist : Employee
+    {
+        public void ShowScientist()
+        {
+            WriteLine("I'm a Scientist!");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public void ShowManager()
+        {
+            WriteLine("I'm a Manager!");
+        }
+    }
+
+    public class Specialist : Employee
+    {
+        public void ShowSpecialist()
+        {
+            WriteLine("I'm a Specialist!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Employee emp = new EmployeeBilder().SetFirstName("IDK").SetLastName("Help").SetBirthDate(DateTime.Now).SetSalary(1500).build();
+            emp.Show();
+
+            Employee emp1 = new EmployeeBilder().SetFirstName("").SetLastName("Help").SetBirthDate(DateTime.MinValue).SetSalary(0).build();
+            emp1.Show();*/
+
+            Employee[] arr = {new Manager(), new Specialist(), new Scientist()};
+            foreach (Employee item in arr)
+            {
+
+                //1
+                try
+                {
+                    ((Manager)item).ShowManager();
+                }
+                catch { }
+
+                //2
+                Specialist a = item as Specialist;
+                if (a != null)
+                {
+                    a.ShowSpecialist();
+                }
+
+                //3
+                if (item is Scientist)
+                {
+                    (item as Scientist).ShowScientist();
+                }
+            }
+        }
+    }
+}
---
+++ Let2/Let2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Runtime.Versioning;
+using static System.Console;
+
+namespace Hell
+{
+    public abstract class Human
+    {
+        string first_name, last_name;
+        DateTime date;
+        public Human() { }
+
+        public Human(string fname, string lname, DateTime d) { }
+        public virtual void Show()
+        {
+            WriteLine($"Фамилия {last_name}, Имя {first_name}, Дата рождения {date.ToShortDateString()}");
+        }
+        public abstract void Think();
+    }
+
+    public abstract class Learner : Human
+    {
+        string institution;
+
+        protected Learner(string fname, string lname, DateTime d, string institution) : base(fname, lname, d)
+        {
+        }
+        public Learner() { }
+        public abstract void Study();
+
+        public virtual void Show()
+        {
+            base.Show();
+            WriteLine($"Institution {institution}");
+        }
+    }
+
+    public class Student : Learner
+    {
+        string groupname;
+        public Student(string fname, string lname, DateTime d, string institution, string groupname) : base(fname, lname, d, institution)
+        {
+        }
+        public Student() { }
+
+        public override void Study()
+        {
+            WriteLine("I study");
+        }
+
+        public override void Think()
+        {
+            WriteLine("I think like student");
+        }
+        public void Show()
+        {
+            base.Show();
+            WriteLine($"Group name {groupname}");
+        }
+    }
+
+    public class SchoolChild : Learner
+    {
+        public SchoolChild(string fname, string lname, DateTime d, string institution) : base(fname, lname, d, institution)
+        {
+        }
+        public SchoolChild() { }
+        public override void Show()
+        {
+            base.Show();
+            WriteLine($"I'm school child");
+        }
+
+        public override void Study()
+        {
+            WriteLine("Am i studying?");
+        }
+
+        public override void Think()
+        {
+            WriteLine("I think like child");
+        }
+    }
+
+    class Pupil : Learner
+    {
+        public Pupil(string fname, string lname, DateTime d, string institution) : base(fname, lname, d, institution)
+        {
+        }
+        public Pupil() { }
+
+        public override void Show()
+        {
+            base.Show();
+            WriteLine("Help pls");
+        }
+
+        public override void Study()
+        {
+            WriteLine("Who?");
+        }
+
+        public override void Think()
+        {
+            WriteLine("What?");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Learner[] arr = { new Student(), new SchoolChild(), new Pupil() };
+            foreach (Learner item in arr)
+            {
+                try
+                {
+                    ((Student)item).Show();
+                }
+                catch
+                {
+
+                }
+
+                if (item is SchoolChild)
+                {
+                    ((SchoolChild)item).Show();
+                }
+
+                Pupil a = item as Pupil;
+                if (a != null)
+                {
+                    a.Show();
+                }
+            }
+        }
+    }
+}
